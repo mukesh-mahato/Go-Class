6ce61919ad4526aa6cbd2efdf0faa3797c5b985b
Mukesh <mukeshmahato2618@gmail.com>
2023-07-16 15:45:38 +0545

    test

---
+++ 02Simple Example/hello_test.go
@@ -0,0 +1,30 @@
+package hello
+
+import "testing"
+
+func TestSayHello(t *testing.T) {
+	subsets := []struct {
+		items  []string
+		result string
+	}{
+		{
+			result: "hello, world!",
+		},
+		{
+			items:  []string{"Mukesh"},
+			result: "hello, Mukesh",
+		},
+		{
+			items:  []string{"Mukesh", "Mahato"},
+			result: "hello, Mukesh, Mahato!",
+		},
+	}
+
+	for _, st := range subsets {
+		if s := Say(st.items); s != st.result {
+			t.Errorf("wanted %s {%v}, got %s", st.result, st.items, s)
+		}
+	}
+}
+
+// go test ./...
